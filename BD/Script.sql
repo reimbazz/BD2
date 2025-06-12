404cbcf010ec886beadb83f4b658f22f9d96fe7b
Pedro Reimberg <pedro.reimberg.oliveira@gmail.com>
2025-06-11 21:32:09 -0300

    Criacao dos usuarios e indices.

--- BD/Script.sql
+++ BD/Script.sql
@@ -97,3 +97,82 @@ ALTER TABLE country_society
 
 ALTER TABLE country_geography
     DROP COLUMN borders;
+
+-- Criacao dos usuarios e permissões
+
+CREATE ROLE documentador;
+CREATE ROLE programador;
+CREATE ROLE dba;
+
+
+CREATE USER hiara WITH PASSWORD '1234';
+GRANT documentador TO hiara;
+
+CREATE USER paulo WITH PASSWORD '1234';
+GRANT programador TO paulo;
+
+CREATE USER reimberg WITH PASSWORD '1234';
+GRANT dba TO reimberg;
+
+CREATE USER joao WITH PASSWORD '1234';
+GRANT programador TO joao;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO documentador;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO programador;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dba WITH GRANT OPTION;
+
+GRANT documentador TO dba WITH ADMIN OPTION;
+GRANT programador TO dba WITH ADMIN OPTION;
+--Criação de índices
+
+-- Tabela countries
+-- O campo country_code já é PK, então já tem índice
+CREATE UNIQUE INDEX idx_country_name ON countries(name);
+
+-- Tabela country_geography
+-- FK
+CREATE INDEX idx_geography_country_code ON country_geography(country_code);
+-- Filtros por região ou área são comuns
+CREATE INDEX idx_geography_region ON country_geography(region);
+CREATE INDEX idx_geography_area ON country_geography(area);
+
+-- Tabela country_society
+-- FK
+CREATE INDEX idx_society_country_code ON country_society(country_code);
+
+-- Busca por capital e população
+CREATE INDEX idx_society_capital ON country_society(capital);
+CREATE INDEX idx_society_population ON country_society(population);
+
+-- Tabela states
+-- FK
+CREATE INDEX idx_states_country_code ON states(country_code);
+
+-- Nome e sigla são importantes para busca
+CREATE INDEX idx_states_name ON states(name);
+CREATE INDEX idx_states_abbreviation ON states(abbreviation);
+
+-- Tabela cities
+-- FK
+CREATE INDEX idx_cities_state_id ON cities(state_id);
+
+-- Nome da cidade e população são comuns em filtros
+CREATE INDEX idx_cities_name ON cities(name);
+CREATE INDEX idx_cities_population ON cities(population);
+
+-- Tabela languages
+-- FK
+CREATE INDEX idx_languages_country_code ON languages(country_code);
+CREATE INDEX idx_languages_language ON languages(language);
+
+-- Tabela currencies
+-- FK
+CREATE INDEX idx_currencies_country_code ON currencies(country_code);
+CREATE INDEX idx_currencies_currency ON currencies(currency);
+
+-- Tabela borders
+-- FKs
+CREATE INDEX idx_borders_country_code ON borders(country_code);
+CREATE INDEX idx_borders_border_country_code ON borders(border_country_code);
