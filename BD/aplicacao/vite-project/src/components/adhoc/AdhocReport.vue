43d375569b810f06d039b8d181c088726673dca0
pvrodrigues10 <paulovitor-cr@hotmail.com>
2025-06-18 23:57:07 -0300

    style: formatacao

--- BD/aplicacao/vite-project/src/components/adhoc/AdhocReport.vue
+++ BD/aplicacao/vite-project/src/components/adhoc/AdhocReport.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue';
-import axios from 'axios';
-import TableSelector from './TableSelector.vue';
-import AttributeSelector from './AttributeSelector.vue';
-import JoinTables from './JoinTables.vue';
-import GroupBy from './GroupBy.vue';
-import OrderBy from './OrderBy.vue';
-import ReportViewer from './ReportViewer.vue';
+import { ref, onMounted, watch } from "vue";
+import axios from "axios";
+import TableSelector from "./TableSelector.vue";
+import AttributeSelector from "./AttributeSelector.vue";
+import JoinTables from "./JoinTables.vue";
+import GroupBy from "./GroupBy.vue";
+import OrderBy from "./OrderBy.vue";
+import ReportViewer from "./ReportViewer.vue";
 
 // Estado da aplicação
 const tables = ref<string[]>([]);
 const tablesJoin = ref<string[]>([]);
-const selectedTable = ref<string>('');
+const selectedTable = ref<string>("");
 const attributes = ref<{ name: string; type: string }[]>([]);
 const selectedAttributes = ref<string[]>([]);
 const joins = ref<any[]>([]);
@@ -20,8 +20,8 @@ const aggregateFunctions = ref<any[]>([]);
 const orderByColumns = ref<any[]>([]);
 const reportData = ref<any[]>([]);
 const isLoading = ref<boolean>(false);
-const error = ref<string>('');
-const sqlQuery = ref<string>('');
+const error = ref<string>("");
+const sqlQuery = ref<string>("");
 const showSql = ref<boolean>(false);
 
 // Carregar tabelas do backend quando o componente for montado
@@ -29,13 +29,13 @@ onMounted(async () => {
   try {
     isLoading.value = true;
     // Buscar as tabelas disponíveis na API
-    const response = await axios.get('http://localhost:8000/api/db/tables');
-    console.log('Tabelas carregadas:', response.data.tables);
+    const response = await axios.get("http://localhost:8000/api/db/tables");
+    console.log("Tabelas carregadas:", response.data.tables);
     tables.value = response.data.tables;
     isLoading.value = false;
   } catch (err) {
-    console.error('Erro ao carregar tabelas:', err);
-    error.value = 'Erro ao carregar tabelas. Tente novamente mais tarde.';
+    console.error("Erro ao carregar tabelas:", err);
+    error.value = "Erro ao carregar tabelas. Tente novamente mais tarde.";
     isLoading.value = false;
   }
 });
@@ -49,11 +49,10 @@ const loadAttributes = async () => {
     selectedAttributes.value = [];
     attributes.value = [];
 
-
     isLoading.value = false;
   } catch (err) {
-    console.error('Erro ao carregar atributos:', err);
-    error.value = 'Erro ao carregar atributos. Tente novamente mais tarde.';
+    console.error("Erro ao carregar atributos:", err);
+    error.value = "Erro ao carregar atributos. Tente novamente mais tarde.";
     isLoading.value = false;
   }
 };
@@ -62,16 +61,19 @@ const loadTablesJoin = async () => {
   try {
     isLoading.value = true;
     // Buscar as tabelas disponíveis para join na API
-    const response = await axios.get(`http://localhost:8000/api/db/tables/${selectedTable.value}/relations`);
+    const response = await axios.get(
+      `http://localhost:8000/api/db/tables/${selectedTable.value}/relations`
+    );
 
     tablesJoin.value = response.data.relations;
     isLoading.value = false;
   } catch (err) {
-    console.error('Erro ao carregar tabelas para join:', err);
-    error.value = 'Erro ao carregar tabelas para join. Tente novamente mais tarde.';
+    console.error("Erro ao carregar tabelas para join:", err);
+    error.value =
+      "Erro ao carregar tabelas para join. Tente novamente mais tarde.";
     isLoading.value = false;
   }
-}
+};
 
 // Observar mudanças na tabela selecionada
 watch(selectedTable, async (newTable) => {
@@ -94,28 +96,25 @@ watch(selectedTable, async (newTable) => {
 // Função para gerar o relatório
 const generateReport = async () => {
   if (!selectedTable.value || selectedAttributes.value.length === 0) {
-    error.value = 'Selecione uma tabela e pelo menos um atributo.';
+    error.value = "Selecione uma tabela e pelo menos um atributo.";
     return;
   }
 
   try {
     isLoading.value = true;
-    error.value = '';
-
+    error.value = "";
 
     isLoading.value = false;
   } catch (err) {
-    console.error('Erro ao gerar relatório:', err);
-    error.value = 'Erro ao gerar relatório. Tente novamente mais tarde.';
+    console.error("Erro ao gerar relatório:", err);
+    error.value = "Erro ao gerar relatório. Tente novamente mais tarde.";
     isLoading.value = false;
   }
 };
 
-
-
 // Limpar todos os filtros e seleções
 const clearAll = () => {
-  selectedTable.value = '';
+  selectedTable.value = "";
   attributes.value = [];
   selectedAttributes.value = [];
   joins.value = [];
@@ -123,8 +122,8 @@ const clearAll = () => {
   aggregateFunctions.value = [];
   orderByColumns.value = [];
   reportData.value = [];
-  error.value = '';
-  sqlQuery.value = '';
+  error.value = "";
+  sqlQuery.value = "";
 };
 </script>
 
@@ -142,31 +141,49 @@ const clearAll = () => {
       </v-col>
 
       <v-col cols="12" md="4">
-        <AttributeSelector :attributes="attributes" v-model:selectedAttributes="selectedAttributes" />
+        <AttributeSelector
+          :attributes="attributes"
+          v-model:selectedAttributes="selectedAttributes"
+        />
       </v-col>
 
       <v-col cols="12" md="4">
-        <JoinTables :tablesFiltersJoin="tablesJoin" :sourceTable="selectedTable" :sourceAttributes="attributes"
-          v-model:joins="joins" />
+        <JoinTables
+          :tablesFiltersJoin="tablesJoin"
+          :sourceTable="selectedTable"
+          :sourceAttributes="attributes"
+          v-model:joins="joins"
+        />
       </v-col>
     </v-row>
 
     <v-row>
       <v-col cols="12" md="6">
-        <GroupBy :attributes="attributes" v-model:groupByAttributes="groupByAttributes"
-          v-model:aggregateFunctions="aggregateFunctions" />
+        <GroupBy
+          :attributes="attributes"
+          v-model:groupByAttributes="groupByAttributes"
+          v-model:aggregateFunctions="aggregateFunctions"
+        />
       </v-col>
 
       <v-col cols="12" md="6">
-        <OrderBy :attributes="attributes" :groupByAttributes="groupByAttributes"
-          v-model:orderByColumns="orderByColumns" />
+        <OrderBy
+          :attributes="attributes"
+          :groupByAttributes="groupByAttributes"
+          v-model:orderByColumns="orderByColumns"
+        />
       </v-col>
     </v-row>
 
     <v-row>
       <v-col cols="12" class="d-flex justify-center my-4">
-        <v-btn color="primary" size="large" @click="generateReport" :loading="isLoading"
-          :disabled="!selectedTable || selectedAttributes.length === 0">
+        <v-btn
+          color="primary"
+          size="large"
+          @click="generateReport"
+          :loading="isLoading"
+          :disabled="!selectedTable || selectedAttributes.length === 0"
+        >
           <v-icon start>mdi-file-chart</v-icon>
           Gerar Relatório
         </v-btn>
@@ -176,9 +193,15 @@ const clearAll = () => {
           Limpar Tudo
         </v-btn>
 
-        <v-btn color="info" size="large" @click="showSql = !showSql" class="ml-4" v-if="sqlQuery">
+        <v-btn
+          color="info"
+          size="large"
+          @click="showSql = !showSql"
+          class="ml-4"
+          v-if="sqlQuery"
+        >
           <v-icon start>mdi-database</v-icon>
-          {{ showSql ? 'Ocultar SQL' : 'Mostrar SQL' }}
+          {{ showSql ? "Ocultar SQL" : "Mostrar SQL" }}
         </v-btn>
       </v-col>
     </v-row>
@@ -199,9 +222,12 @@ const clearAll = () => {
 
     <v-row>
       <v-col cols="12">
-        <ReportViewer :reportData="reportData" :isLoading="isLoading" :error="error" />
+        <ReportViewer
+          :reportData="reportData"
+          :isLoading="isLoading"
+          :error="error"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
-
